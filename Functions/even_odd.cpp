e3154c5e820a5a3c308adccc35f027bdfff15723
anushika0609 <anushkasaini0609@gmail.com>
2025-08-06 23:09:08 +0530

    add new file

---
+++ Functions/even_odd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void check(int num){
+    if(num%2==0){
+        cout<<num<<" is even";
+    }
+    else{
+        cout<<num<<" is odd";
+    }
+}
+
+int main(){
+    int num;
+    cout<<"Enter number : ";
+    cin>>num;
+
+    check(num);
+}
