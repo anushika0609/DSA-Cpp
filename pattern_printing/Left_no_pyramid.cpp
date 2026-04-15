33497c0e38f6b2340a0ae4e33c1e75da6b452242
anushika0609 <anushkasaini0609@gmail.com>
2026-04-15 18:04:08 +0530

    add new file

--- pattern_printing/Left_no_pyramid.cpp
+++ pattern_printing/Left_no_pyramid.cpp
@@ -7,7 +7,7 @@ int main(){
     cin>>n;
 
     for(int i=2; i<=n+1; i++){
-        for(int j=1; j<i; j++){
+        for(int j=1 ; j<i; j++){
             cout<<j<<" ";
         }
         cout<<endl;
